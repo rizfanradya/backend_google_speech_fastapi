40ab2176848da8ba7ccda8614611e9e4a0f92054
rizfanradya <rizfankusuma@gmail.com>
2025-02-27 12:09:10 +0700

    rmv orp files

--- utils/remove_orphaned_files.py
+++ utils/remove_orphaned_files.py
@@ -1,20 +1,50 @@
 import os
+from .database import get_db
+from models.speech_result import SpeechResult
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 async def check_and_remove_orphaned_files():
+    print("Check and Remove Orphaned Files Start...")
     abs_path = os.path.abspath(__file__)
     base_dir = os.path.dirname(os.path.dirname(abs_path))
-    # async with get_db_session() as session:
+    async for get_session in get_db():
+        session: AsyncSession = get_session
 
-    #     Check and remove orphaned uploaded files in Masterplan data
-    #     data_in_db = [file[0] for file in session.query(Masterplan.uploaded_file).where(
-    #         Masterplan.uploaded_file.isnot(None)).all()]
-    #     directory = os.path.join(base_dir, 'data', 'uploads')
-    #     os.makedirs(directory, exist_ok=True)
-    #     files_delete = [
-    #         f for f in os.listdir(directory) if f not in data_in_db
-    #     ]
-    #     for file_name in files_delete:
-    #         file_path = os.path.join(directory, file_name)
-    #         if os.path.isfile(file_path):
-    #             os.remove(file_path)
+        # Check and remove orphaned uploaded files in Speech Result data
+        query_db = await session.execute(
+            select(SpeechResult).where(
+                SpeechResult.input_file_audio.isnot(None))
+        )
+        data_in_db = [row.input_file_audio for row in query_db.scalars().all()]
+        directory = os.path.join(
+            base_dir, 'data', 'uploads', 'input_file_audio')
+        os.makedirs(directory, exist_ok=True)
+        files_delete = [
+            f for f in os.listdir(directory) if f not in data_in_db
+        ]
+        for file_name in files_delete:
+            file_path = os.path.join(directory, file_name)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+
+        # Check and remove orphaned downloaded files in Speech Result data
+        query_db = await session.execute(
+            select(SpeechResult).where(
+                SpeechResult.output_file_audio.isnot(None))
+        )
+        data_in_db = [
+            row.output_file_audio for row in query_db.scalars().all()]
+        directory = os.path.join(
+            base_dir, 'data', 'downloads', 'output_file_audio')
+        os.makedirs(directory, exist_ok=True)
+        files_delete = [
+            f for f in os.listdir(directory) if f not in data_in_db
+        ]
+        for file_name in files_delete:
+            file_path = os.path.join(directory, file_name)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+
+    print("Check and Remove Orphaned Files Done...")
